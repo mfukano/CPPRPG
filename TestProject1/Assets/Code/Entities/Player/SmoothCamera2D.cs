2c15c320367854504967449c8bdae65ee8152ab1
CraigCollins <craigcollinsjr2@gmail.com>
2015-01-28 20:34:44 -0800

    Changed Asset Size and Camera things

--- TestProject1/Assets/Code/Entities/Player/SmoothCamera2D.cs
+++ TestProject1/Assets/Code/Entities/Player/SmoothCamera2D.cs
@@ -17,24 +17,24 @@ public class SmoothCamera2D : MonoBehaviour {
 	public Transform target;
 	
 	// Update is called once per frame
-	void Update () 
-	{
-		if (target)
-		{
-			Vector3 point = camera.WorldToViewportPoint(target.position);
-			Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
-			Vector3 destination = transform.position + delta;
-			cameraTruePos = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
-			transform.position = cameraTruePos; //+ new Vector3( -0.1f, -0.1f, 0f);
-
-			//var v = target.position;
-			//transform.position = Vector3.MoveTowards(transform.position, v, 10 * Time.deltaTime);
-			//var v = target.position;
-			//transform.position = new Vector3 (v.x, v.y, transform.position.z);
-		}
-
-		
-	}
+//	void Update () 
+//	{
+//		if (target)
+//		{
+//			Vector3 point = camera.WorldToViewportPoint(target.position);
+//			Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+//			Vector3 destination = transform.position + delta;
+//			cameraTruePos = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+//			transform.position = cameraTruePos; //+ new Vector3( -0.1f, -0.1f, 0f);
+//
+//			//var v = target.position;
+//			//transform.position = Vector3.MoveTowards(transform.position, v, 10 * Time.deltaTime);
+//			//var v = target.position;
+//			//transform.position = new Vector3 (v.x, v.y, transform.position.z);
+//		}
+//
+//		
+//	}
 
 	void LateUpdate()
 	{
