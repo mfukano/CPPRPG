7dbc397faa73ef9278236d0b33475e56569f1dad
mfukano <fukano.mat@gmail.com>
2015-02-04 17:51:03 -0800

    Final maps included
    Square versions to be bounded and transitions added

---
+++ TestProject1/Assets/Code/Entities/Camera/SmoothCamera2D.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothCamera2D : MonoBehaviour {
+
+	public int ratio;
+	private Map myMap;
+
+	// Use this for initialization
+	void Start () {
+		camera.orthographicSize = Screen.height / (2 * ratio);
+	
+		myMap = (Map)GameObject.FindObjectOfType (typeof(Map));
+	}
+
+	public float dampTime = 0.2f;
+	private Vector3 velocity = Vector3.zero;
+	private Vector3 cameraTruePos;
+	public Transform target;
+	
+	// Update is called once per frame
+//	void Update () 
+//	{
+//		if (target)
+//		{
+//			Vector3 point = camera.WorldToViewportPoint(target.position);
+//			Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+//			Vector3 destination = transform.position + delta;
+//			cameraTruePos = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+//			transform.position = cameraTruePos; //+ new Vector3( -0.1f, -0.1f, 0f);
+//
+//			//var v = target.position;
+//			//transform.position = Vector3.MoveTowards(transform.position, v, 10 * Time.deltaTime);
+//			//var v = target.position;
+//			//transform.position = new Vector3 (v.x, v.y, transform.position.z);
+//		}
+//
+//		
+//	}
+
+	void LateUpdate()
+	{
+		if (target)
+		{
+			Vector3 point = camera.WorldToViewportPoint(target.position);
+			Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+			Vector3 destination = transform.position + delta;
+				
+			cameraTruePos = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+			//transform.position = cameraTruePos; //+ new Vector3( -0.1f, -0.1f, 0f);
+			transform.position = new Vector3(
+				Mathf.Clamp(cameraTruePos.x, -myMap.width / 2 + camera.pixelWidth / 2, myMap.width / 2 - camera.pixelWidth / 2),
+				Mathf.Clamp(cameraTruePos.y, -myMap.height / 2 + camera.pixelHeight / 2, myMap.height / 2 - camera.pixelHeight / 2),
+				cameraTruePos.z);
+			
+			//var v = target.position;
+			//transform.position = new Vector3 (Mathf.Round(v.x), Mathf.Round(v.y), Mathf.Round(transform.position.z));
+		}
+	}
+}
---
+++ TestProject1/Assets/Code/Equippables/Equippables.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Equippables : MonoBehaviour {
+	public float eWeight;
+	public float Damage;
+	// Use this for initialization
+	void Start () {
+		rigidbody2D.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
