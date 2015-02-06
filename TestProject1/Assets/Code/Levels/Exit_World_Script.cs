51a814a1921ebf44f798eb91d4da6270a6701dfe
CraigCollins <craigcollinsjr2@gmail.com>
2015-02-05 18:51:15 -0800

    Added The map exit scripts again

---
+++ TestProject1/Assets/Code/Levels/Exit_World_Script.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Exit_World_Script : MonoBehaviour {
+	
+	public Player player_0;
+	
+	void Start() {
+		player_0 = (Player)GameObject.FindObjectOfType (typeof(Player));
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter2D(Collider2D unit) {
+		if (unit.gameObject.tag == "Player"){
+			//Application.LoadLevel("Aridae");
+		}
+	}
+}
---
+++ TestProject1/Assets/Code/Levels/Map.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Map : MonoBehaviour {
+	
+	public int width;
+	public int height;
+	public Texture2D mapTexture;
+	
+	void Start() {
+		width = mapTexture.width;
+		height = mapTexture.height;
+	}
+}
