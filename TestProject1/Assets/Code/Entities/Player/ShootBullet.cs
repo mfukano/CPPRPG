a81c7921434ecd000bdf83c5b9173b206c6226d9
camgravel <cam.gravel@gmail.com>
2015-02-10 14:23:15 -0800

    player shooting

---
+++ TestProject1/Assets/Code/Entities/Player/ShootBullet.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootBullet : MonoBehaviour {
+
+	public float bulletSpeed;
+	// Use this for initialization
+	void Start () {
+		rigidbody2D.AddForce (Vector3.forward * bulletSpeed);
+	}
+}
