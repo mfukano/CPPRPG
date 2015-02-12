a23effb559fbf6426d4b222b323ae1aad71ee966
camgravel <cam.gravel@gmail.com>
2015-02-12 13:46:11 -0800

    8 direction movement, NOT NORMALIZED

--- TestProject1/Assets/Code/Entities/Player/PlayerNZMovement.cs
+++ TestProject1/Assets/Code/Entities/Player/PlayerNZMovement.cs
@@ -3,43 +3,42 @@ using System.Collections;
 
 public class PlayerNZMovement : MonoBehaviour {
 
+	private float walkSpeed;
+	private float curSpeed;
+	private float maxSpeed;
+
 	public float playerSpeed;
 	public float bulletSpeed;
+	public bool bulletInitVel;
 	Animator anim;
 
 	void Start() {
 		anim = GetComponent<Animator>();
+
+		walkSpeed = 300;
 	}
 
 	void FixedUpdate() {
-		lookAtMouse ();
-		move ();
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		transform.rotation = Quaternion.LookRotation (Vector3.forward, mousePos - transform.position);
+		rigidbody2D.velocity = new Vector2 (Mathf.Lerp (0, Input.GetAxis ("Horizontal") * playerSpeed, 0.8f),
+			                                Mathf.Lerp (0, Input.GetAxis ("Vertical") * playerSpeed, 0.8f));
+		rigidbody2D.angularVelocity = 0;
+
 	}
 
 	void Update() {
-		updateAnim ();
-	}
-
-	void lookAtMouse() {
-		Vector3 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-		transform.rotation = Quaternion.LookRotation (Vector3.forward, mousePos - transform.position);
-	}
-
-	void move() {
-		float input = Input.GetAxis ("Vertical");
-		rigidbody2D.AddForce (gameObject.transform.up * playerSpeed * input);
-		rigidbody2D.angularVelocity = 0;
-	}
-
-	void updateAnim() {
 		if (Input.GetMouseButtonDown (1)) {
 			anim.SetTrigger ("Attack");
 		}
 		if (Input.GetMouseButtonDown (0)) {
 			GameObject bullet = Instantiate(Resources.Load("bullet"), transform.position, transform.rotation) as GameObject;
-			bullet.rigidbody2D.AddForce (bullet.transform.up * (bulletSpeed + (playerSpeed * Input.GetAxis ("Vertical"))));
+			if (bulletInitVel)
+				bullet.rigidbody2D.AddForce (bullet.transform.up * (bulletSpeed + (playerSpeed * Input.GetAxis ("Vertical"))));
+			else
+				bullet.rigidbody2D.AddForce (bullet.transform.up * bulletSpeed);
 		}
 		float input = Input.GetAxis ("Vertical");
-		anim.SetFloat ("Speed", playerSpeed * input);
+		anim.SetFloat ("Speed", rigidbody2D.velocity.magnitude);
 	}
 }
