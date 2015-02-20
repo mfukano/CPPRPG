035af411706f0ef50bb39c0f9b33c739d6fa640b
CraigCollins <craigcollinsjr2@gmail.com>
2015-02-19 23:21:20 -0800

    Added Craig Scene and basic AI scripts
    Doesn’t work yet.

---
+++ TestProject1/Assets/Code/Entities/Enemy/Enemy.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+	
+	public float maxHealth = 1000;
+	public float startHealth;
+	public float currentHealth;
+	
+	public float enemySpeed;
+	private float velMag;
+	private bool isDead;
+	Animator anim;
+
+	// AI
+	public Enemy_AI_Ranged myAI;
+	
+	void Start() {
+		anim = GetComponent<Animator>();
+		startHealth = maxHealth;
+		currentHealth = startHealth;
+		isDead = false;
+
+		// Create AI
+		myAI = new Enemy_AI_Ranged ();
+		myAI.Owner = this;
+	}
+	
+	void FixedUpdate() {
+
+	}
+	
+	public void takeDamage (float dmg_val) {
+		currentHealth -= dmg_val;
+		
+		if (currentHealth <= 0) {
+			Death();
+		}
+	}
+	
+	void Death(){
+		//TODO: Remove charater here
+		isDead = true;
+		enemySpeed = 0;
+		
+	}
+
+	
+/*	void Update() {
+		velMag = rigidbody2D.velocity.magnitude;
+		if (Input.GetMouseButtonDown (1)) {
+			anim.SetTrigger ("Attack");
+		}
+		if (Input.GetMouseButtonDown (0) && Time.timeScale == 1) {
+			GameObject bullet = Instantiate(Resources.Load("bullet"), transform.position, transform.rotation) as GameObject;
+			if (bulletInitVel)
+				bullet.rigidbody2D.AddForce (bullet.transform.up * (bulletSpeed + (playerSpeed * Input.GetAxis ("Vertical"))));
+			else
+				bullet.rigidbody2D.AddForce (bullet.transform.up * bulletSpeed);
+		}
+		float input = Input.GetAxisRaw ("Vertical");
+		anim.SetFloat ("Speed", rigidbody2D.velocity.magnitude);
+	} */
+}
+
+
---
+++ TestProject1/Assets/Code/Entities/Enemy/Enemy_AI/Enemy_AI_Ranged.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_AI_Ranged : MonoBehaviour {
+
+	// MyOwner
+	public Enemy Owner;
+
+	// Location Vars
+	public bool DoISeePlayer;
+	public Transform LastKnownLocation;
+	public Transform TargetLocation;
+	public Player TargetPlayer;
+
+	// Use this for initialization
+	void Start () {
+		Owner = (Enemy)gameObject.GetComponentInParent (typeof(Enemy));
+		TargetPlayer = (Player)GameObject.FindObjectOfType (typeof(Player));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Going to try to make the enemy look at the player
+
+	}
+}
