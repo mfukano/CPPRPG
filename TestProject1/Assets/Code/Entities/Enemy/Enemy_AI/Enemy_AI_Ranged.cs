8cbaec770b047b41f57d1b11f1bac5670e22e831
CraigCollins <craigcollinsjr2@gmail.com>
2015-02-24 19:26:01 -0800

    Ai code for finding the player.
    Not finished yet.

--- TestProject1/Assets/Code/Entities/Enemy/Enemy_AI/Enemy_AI_Ranged.cs
+++ TestProject1/Assets/Code/Entities/Enemy/Enemy_AI/Enemy_AI_Ranged.cs
@@ -8,9 +8,22 @@ public class Enemy_AI_Ranged : MonoBehaviour {
 
 	// Location Vars
 	public bool DoISeePlayer;
-	public Transform LastKnownLocation;
+	public Vector3 LastKnownLocation;
 	public Transform TargetLocation;
 	public Player TargetPlayer;
+
+	// Visual Variables
+	public float fieldOfViewAngle = 110;
+	public bool playerInSight;
+	public Vector3 personalLastKnownLocation;
+	private GameObject player;
+	private Animator playerAnim;
+	private Vector3 previousSighting;
+
+	void Awake()
+	{
+
+	}
 
 	// Use this for initialization
 	void Start () {
@@ -24,5 +37,19 @@ public class Enemy_AI_Ranged : MonoBehaviour {
 		DoISeePlayer = true;
 		TargetLocation = TargetPlayer.transform;
 		Owner.transform.rotation = Quaternion.LookRotation (Vector3.forward, TargetLocation.position - transform.position);
+		if (LastKnownLocation != previousSighting)
+			personalLastKnownLocation = LastKnownLocation;
+		previousSighting = LastKnownLocation;
+
+		//TODO: Check player health here
+	}
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		// Player is hidden by default
+		playerInSight = false;
+
+		Vector3 direction = other.transform.position - transform.position;
+		float angle = Vector3.Angle (direction, transform.forward);
 	}
 }
