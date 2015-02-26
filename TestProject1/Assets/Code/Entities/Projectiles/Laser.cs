e7e59aaadfdbbb870cf5f0a10252e299d19853d0
camgravel <cam.gravel@gmail.com>
2015-02-25 16:22:45 -0800

    projectile types

---
+++ TestProject1/Assets/Code/Entities/Projectiles/Laser.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Laser : MonoBehaviour {
+	
+	void OnCollisionEnter2D(Collision2D col) {
+		if (col.gameObject.tag == "Wall" || col.gameObject.tag == "Enemy") {
+			Destroy (gameObject);
+		}
+	}
+	
+}
