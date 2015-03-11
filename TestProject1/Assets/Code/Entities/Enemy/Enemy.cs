e157e426d113446234389a0199bad63aded3d753
camgravel <cam.gravel@gmail.com>
2015-03-10 19:52:46 -0700

    lightsaber. ammo from enemies update

--- TestProject1/Assets/Code/Entities/Enemy/Enemy.cs
+++ TestProject1/Assets/Code/Entities/Enemy/Enemy.cs
@@ -59,11 +59,8 @@ public class Enemy : MonoBehaviour {
 		isDead = true;
 		enemySpeed = 0;
 		Destroy (gameObject);
-		
-	}
-
-	void OnDestroy() {
 		GameObject ammo = Instantiate(Resources.Load("Prefabs/Items/ammo"), transform.position, transform.rotation) as GameObject;
+		
 	}
 
 	void OnCollisionEnter2D(Collision2D col) {
