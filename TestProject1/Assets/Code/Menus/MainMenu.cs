f091d795106a5683c40404c4a4890fea5361c105
mfukano <fukano.mat@gmail.com>
2015-03-11 20:56:33 -0700

    Player character spawns only once
    In the worst, dirtiest of ways

--- TestProject1/Assets/Code/Menus/MainMenu.cs
+++ TestProject1/Assets/Code/Menus/MainMenu.cs
@@ -8,8 +8,11 @@ public class MainMenu : MonoBehaviour {
 	public float buttonHeight = 0.12f;
 	public float pos = 0.25f;
 	public GUIStyle style;
+	Player myPlayer;
+
 
 	void Start() {
+		myPlayer = (Player)GameObject.FindObjectOfType (typeof(Player));
 		style.normal.textColor = Color.white;
 		style.alignment = TextAnchor.UpperCenter;
 		style.fontSize = 30;
@@ -20,6 +23,7 @@ public class MainMenu : MonoBehaviour {
 		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), backgroundTexture);
 		//display menu buttons
 		if (GUI.Button (new Rect (Screen.width*pos, (float)Screen.height*1.75F*pos, Screen.width*buttonWidth, (float)Screen.height*0.75F*buttonHeight), "New Game")){
+			DontDestroyOnLoad(myPlayer);
 			Application.LoadLevel("Aridae_Final");
 		}
 		if (GUI.Button (new Rect (Screen.width * pos, (float)Screen.height*2.5F* pos, Screen.width * buttonWidth, (float)Screen.height*0.75F*buttonHeight), "Test Suite")) {
